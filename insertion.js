85febb74ff7c3e7a05c17d03c6bec72eb78464df
Taioh Kubota <kubotataioh@gmail.com>
2022-09-12 15:11:33 -0400

    finished functions, but not further study

---
+++ insertion.js
@@ -0,0 +1,10 @@
+function insertionSort(arr) {
+    for(let i = 0; i<arr.length; i++){
+        let current = arr[i];
+        for(let j =0; j<arr.length; j++){
+
+        }
+    }
+}
+
+module.exports = insertionSort;
